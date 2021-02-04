239b68a09a92982853b18a327ccd941de252178e
veetance <divineokos@hotmail.com>
2021-02-04 05:53:59 -0700

    .js linked and mouse click logo to see new logo implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 // console.log("hello!") testing if .js worked//
+
+
+$(".joke").text ("What Bow can't be tied?")
+
+var $one = $("#l1");
+var $two = $("#l2");
+var $three = $("#l3");
+var $four = $("#l4");
+
+
+$one.on("click", function() {
+	$(this).hide();
+	$two.show();
+})
+
+$two.on("click", function() {
+	$(this).hide();
+	$three.show();
+})
+
+$three.on("click", function() {
+	$(this).hide();
+	$four.show();
+})
+
+$four.on("click", function() {
+	$(this).hide();
+	$one.show();
+})
